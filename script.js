f03ec6429f3820b32c5dfcbd85c125437c9d4dc0
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 20:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  
+  let lower = str.toLowerCase();
+  
+  
+  let frequency = {};
+
+  
+  for (let i = 0; i < lower.length; i++) {
+    let currentChar = lower[i];
+    frequency[currentChar] = (frequency[currentChar] || 0) + 1;
+  }
+
+  for (let i = 0; i < lower.length; i++) {
+    if (frequency[lower[i]] === 1) {
+      return lower[i];  
+    }
+  }
+
+  
+  return "null";
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
